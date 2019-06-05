8cb16a68203cd160a32db034060ad7d724e8d1a8
fkxie <13857501867@sina.cn>
2019-06-05 15:07:18 +0800

    initial

---
+++ cpp_learning/cpp_learning/main.cpp
@@ -0,0 +1,33 @@
+//
+//
+//  Created by fkxie on 2019/5/6.
+//  Copyright © 2019年 fkxie. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cassert>
+
+#define MAXPATH 80
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    string file="/Users/fkxie/Desktop/Directory/GAP/devel/POSCAR";
+    fstream infile;
+    infile.open(file.data());
+    cout<<infile.is_open()<<endl;
+    assert(infile.is_open());
+    string s;
+    while (getline(infile, s))
+    {
+        cout<<s<<endl;
+    }
+    infile.close();
+    
+    cout << "test"<< endl;
+    cout << "pic" << endl;
+    cout << "k" << endl;
+    return 0;
+}
